ec7c5cce38a6777e8f9ca3138fb0a265846d20a6
Piotr-programmer <piotr.jaskiewicz91@gmail.com>
2021-04-15 21:24:07 +0200

    commit

--- auto_tests.py
+++ auto_tests.py
@@ -1,17 +1,14 @@
 from selenium import webdriver
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox(executable_path="C:\TestFiles\geckodriver.exe")
-driver.get('http://eve.ii.pw.edu.pl:9007/#/start_page')
-title = driver.title
-print(title)
-assert 'DnaAssembler' == title
-time.sleep(2)
-text = driver.findElement(By.xpath("/(@class,'ng-binding')/h1")).getText()
-print(text)
+from datetime import datetime
 
+from test import driver
 
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+string8 = driver.find_element_by_id("server_time").text
+print(string8)
+print(current_time)
 
-
+assert string8 == current_time
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 import time
+from datetime import datetime
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-
 
 driver = webdriver.Firefox(executable_path="C:\TestFiles\geckodriver.exe")
 driver.get('http://eve.ii.pw.edu.pl:9007/#/start_page')
@@ -13,19 +13,140 @@ time.sleep(2)
 
 string1 = driver.find_element_by_tag_name("h1").text
 assert string1 == "DnaAssembler - Help"
-string2 = driver.find_element_by_tag_name("h3").text
-assert string2 == "Authors"
-string3 = driver.find_element_by_id("file_formats_href").text
-assert string3 == "Input and output file formats"
-string4 = driver.find_element_by_id("algorithms_href").text
-assert string4 == "Parameters and algorithms used in application"
 
+string2 = driver.find_element_by_id("file_formats_href").text
+assert string2 == "Input and output file formats"
 
-string6 = driver.find_element_by_id("client_version_val").text
-print(string6)
+string3 = driver.find_element_by_id("algorithms_href").text
+assert string3 == 'Parameters and algorithms used in application'
 
-string7 = driver.find_element_by_id("right_col").text
-print(string7)
+string4 = driver. find_element_by_xpath("//body/div[1]/div[2]/div[2]/h4[5]").text
+assert string4 == "For more information send an email:\nr.m.nowak@elka.pw.edu.pl"
+
+string5 = driver.find_element_by_xpath("//h3[contains(text(),'Authors')]").text
+assert string5 == "Authors"
+
+string6 = driver.find_element_by_xpath("//span[@id='authors']").text
+assert string6 == "Robert Nowak - r.m.nowak@elka.pw.edu.pl\nWiktor Kuśmirek\n\nWarsaw University of Technology,\nFaculty of Electronics and Information Technology,\nWarsaw 2015"
+
+string7 = driver.find_element_by_xpath("//h3[contains(text(),'Application parameters')]").text
+assert string7 == "Application parameters"
+
+#now = datetime.now()
+#current_time = now.strftime("%H:%M:%S")
+#string8 = driver.find_element_by_id("server_time").text
+#print(string8)
+#print(current_time)
+#assert string8 == current_time
+
+string9 = driver.find_element_by_xpath("//li[@id='db_version']").text
+assert string9 == "db version: PostgreSQL 9.4.18 on x86_64-unknown-linux-gnu"
+
+string10 = driver.find_element_by_id("server_version").text
+assert string10 == "server version: 0.07.1635; Python: 3.5.3; Arch: ; Os: Linux #1 SMP Debian 4.9.110-1 (2018-07-05); Django: 2.0.2"
+
+string11 = driver.find_element_by_xpath("//li[@id='client_version']").text
+assert string11 == "client version: 0.06.1634"
+
+string12 = driver.find_element_by_xpath("//legend[@id='demo_files_legend']").text
+assert string12 == "Files from demo"
+
+string13 = driver.find_element_by_xpath("//a[@id='reads_PET_R1.fq_href']").text
+assert string13 == "reads_PET_R1.fq"
+
+string14 = driver.find_element_by_xpath("//a[@id='reads_PET_R2.fq_href']").text
+assert string14 == "reads_PET_R2.fq"
+
+string15 = driver.find_element_by_xpath("//a[@id='ref.fa_href']").text
+assert string15 == "ref.fa"
+
+string16 = driver.find_element_by_xpath("//a[@id='seq.fa_href']").text
+assert string16 == "seq.fa"
+
+string17 = driver.find_element_by_xpath("//a[@id='cmp.vcf_href']").text
+assert string17 == "cmp.vcf"
+
+button1 = driver.find_element_by_id("showHelpWindowButton").text
+assert button1 == "Help"
+
+button2 = driver.find_element_by_id("showLoginWindowButton").text
+assert button2 == "Log in"
+
+button3 = driver.find_element_by_id("showNewUserWindowButton").text
+assert button3 == "Create new user"
+
+button4 = driver.find_element_by_id("loginAsGuestButton").text
+assert button4 == "Log in as guest"
+###########################PL###########################################
+button = driver.find_element_by_xpath("//body/div[1]/div[1]/div[1]/ul[1]/li[2]/a[1]/img[1]")
+button.click()
+
+string1pl = driver.find_element_by_tag_name("h1").text
+assert string1pl == "DnaAssembler - Pomoc"
+
+string2pl = driver.find_element_by_id("file_formats_href").text
+assert string2pl == "Formaty plików wejściowych i wyjściowych"
+
+string3pl = driver.find_element_by_id("algorithms_href").text
+assert string3pl == 'Parametry i algorytmy wykorzystane w aplikacji'
+
+string4pl = driver. find_element_by_xpath("//body/div[1]/div[2]/div[2]/h4[5]").text
+assert string4pl == "Uzyskaj więcej informacji wysyłając wiadomość:\nr.m.nowak@elka.pw.edu.pl"
+
+string5pl = driver.find_element_by_xpath("//h3[contains(text(),'Autorzy')]").text
+assert string5pl == "Autorzy"
+
+string6pl = driver.find_element_by_xpath("//span[@id='authors']").text
+assert string6pl == "Robert Nowak - r.m.nowak@elka.pw.edu.pl\nWiktor Kuśmirek\n\nPolitechnika Warszawska,\nWydział Elektroniki i Technik Informacyjnych,\nWarszawa 2015"
+
+string7pl = driver.find_element_by_xpath("//h3[contains(text(),'Parametry aplikacji')]").text
+assert string7pl == "Parametry aplikacji"
+
+#now = datetime.now()
+#current_time = now.strftime("%H:%M:%S")
+#string8 = driver.find_element_by_id("server_time").text
+#print(string8)
+#print(current_time)
+#assert string8 == current_time
+
+string9pl = driver.find_element_by_xpath("//li[@id='db_version']").text
+assert string9pl == "wersja bazy danych: PostgreSQL 9.4.18 on x86_64-unknown-linux-gnu"
+
+string10 = driver.find_element_by_id("server_version").text
+assert string10 == "wersja serwera: 0.07.1635; Python: 3.5.3; Arch: ; Os: Linux #1 SMP Debian 4.9.110-1 (2018-07-05); Django: 2.0.2"
+
+string11pl = driver.find_element_by_xpath("//li[@id='client_version']").text
+assert string11pl == "wersja klienta: 0.06.1634"
+
+string12pl = driver.find_element_by_xpath("//legend[@id='demo_files_legend']").text
+assert string12pl == "Pliki z filmu"
+
+string13pl = driver.find_element_by_xpath("//a[@id='reads_PET_R1.fq_href']").text
+assert string13pl == "reads_PET_R1.fq"
+
+string14pl = driver.find_element_by_xpath("//a[@id='reads_PET_R2.fq_href']").text
+assert string14pl == "reads_PET_R2.fq"
+
+string15pl = driver.find_element_by_xpath("//a[@id='ref.fa_href']").text
+assert string15pl == "ref.fa"
+
+string16pl = driver.find_element_by_xpath("//a[@id='seq.fa_href']").text
+assert string16pl == "seq.fa"
+
+string17pl = driver.find_element_by_xpath("//a[@id='cmp.vcf_href']").text
+assert string17pl == "cmp.vcf"
+
+button1 = driver.find_element_by_id("showHelpWindowButton").text
+assert button1 == "Pomoc"
+
+button2 = driver.find_element_by_id("showLoginWindowButton").text
+assert button2 == "Zaloguj się"
+
+button3 = driver.find_element_by_id("showNewUserWindowButton").text
+assert button3 == "Dodaj użytkownika"
+
+button4 = driver.find_element_by_id("loginAsGuestButton").text
+assert button4 == "Zaloguj się jako gość"
 
 
 
